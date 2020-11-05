523b911843b402e19f81422d0f657094dec4db4a
Cyphall <cyphallbraus@gmail.com>
2020-11-05 18:18:25 +0100

    Fixed back screen due to previous commit

--- src/GLObject/CreateInfo/CubemapCreateInfo.h
+++ src/GLObject/CreateInfo/CubemapCreateInfo.h
@@ -10,5 +10,5 @@ struct CubemapCreateInfo
 	glm::ivec2 size;
 	GLenum internalFormat = GL_RGB8;
 	GLenum textureFiltering = GL_NEAREST;
-	std::array<GLint, 4> swizzle;
+	std::array<GLint, 4> swizzle = {GL_RED, GL_GREEN, GL_BLUE, GL_ALPHA};
 };
