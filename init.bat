a3eba97d8b00eb9bc0d56264f9e8305bd1e89ae8
Cyphall <cyphallbraus@gmail.com>
2021-08-12 00:21:56 +0200

    misc: froze vcpkg dependencies versions

--- init.bat
+++ init.bat
@@ -2,6 +2,7 @@
 set VCPKG_DEFAULT_TRIPLET=x64-windows-static
 git clone https://github.com/Microsoft/vcpkg
 cd vcpkg
+git checkout 69478c5caafcde4c490bb1fccb960296801dbb5f
 call bootstrap-vcpkg.bat
 vcpkg install glfw3 glm nlohmann-json assimp stb magic-enum
 rmdir /s /q .git
