8ca5fa53f18a4fc60d16831748ad4469d3b2c3f5
Cyphall <cyphallbraus@gmail.com>
2022-07-30 19:40:26 +0200

    misc: updated vcpkg version

--- init.bat
+++ init.bat
@@ -2,7 +2,7 @@
 set VCPKG_DEFAULT_TRIPLET=x64-windows-static
 git clone https://github.com/Microsoft/vcpkg
 cd vcpkg
-git checkout 69478c5caafcde4c490bb1fccb960296801dbb5f
+git checkout 66045de4dcc5da3d1029c02b606307f5951dcb22
 call bootstrap-vcpkg.bat
 vcpkg install glfw3 glm nlohmann-json assimp stb magic-enum
 rmdir /s /q .git
