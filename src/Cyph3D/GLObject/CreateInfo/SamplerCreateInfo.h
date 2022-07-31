733bfac4438523487df1e010b32a8e1d2eb33051
Cyphall <cyphallbraus@gmail.com>
2022-07-31 22:51:32 +0200

    fix: fixed SamplerCreateInfo.h missing an include and not using GL types

--- src/Cyph3D/GLObject/CreateInfo/SamplerCreateInfo.h
+++ src/Cyph3D/GLObject/CreateInfo/SamplerCreateInfo.h
@@ -1,18 +1,19 @@
 #pragma once
 
+#include <glad/glad.h>
 #include <array>
 
 struct SamplerCreateInfo
 {
 	GLenum minFilter = GL_NEAREST_MIPMAP_LINEAR;
 	GLenum magFilter = GL_LINEAR;
-	float minLod = -1000;
-	float maxLod = 1000;
-	float lodBias = 0;
+	GLfloat minLod = -1000;
+	GLfloat maxLod = 1000;
+	GLfloat lodBias = 0;
 	GLenum wrapS = GL_REPEAT;
 	GLenum wrapT = GL_REPEAT;
 	GLenum wrapR = GL_REPEAT;
-	std::array<float, 4> borderColor = {0.0f, 0.0f, 0.0f, 0.0f};
+	std::array<GLfloat, 4> borderColor = {0.0f, 0.0f, 0.0f, 0.0f};
 	GLenum compareFunc = GL_NONE;
-	bool anisotropicFiltering = false;
+	GLboolean anisotropicFiltering = false;
 };
