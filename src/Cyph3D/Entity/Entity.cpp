944e3e01c4239b8215fcc7ef65fe92790d0d20d2
Cyphall <cyphallbraus@gmail.com>
2022-09-15 22:33:11 +0200

    refactor: moved entire entity inspector UI drawing to Entity::onDrawUi

--- src/Cyph3D/Entity/Entity.cpp
+++ src/Cyph3D/Entity/Entity.cpp
@@ -195,6 +195,29 @@ void Entity::onDrawUi()
 			it = removeComponent(it);
 		}
 	}
+
+	ImGui::Spacing();
+	ImGui::Separator();
+	ImGui::Spacing();
+
+	float availableWidth = ImGui::GetWindowContentRegionWidth();
+	float buttonWidth = std::min(180.0f, availableWidth);
+	ImGui::SetCursorPosX(ImGui::GetCursorPosX() + ((availableWidth - buttonWidth) / 2));
+
+	if (ImGui::Button("Add Component", ImVec2(buttonWidth, 0)))
+		ImGui::OpenPopup("add_component");
+
+	if (ImGui::BeginPopup("add_component"))
+	{
+		for (auto it = Entity::allocators_begin(); it != Entity::allocators_end(); it++)
+		{
+			if (ImGui::Selectable(it->first.c_str()))
+			{
+				it->second(*this);
+			}
+		}
+		ImGui::EndPopup();
+	}
 	
 	ImGui::PopID();
 }
--- src/Cyph3D/UI/Window/UIInspector.cpp
+++ src/Cyph3D/UI/Window/UIInspector.cpp
@@ -1,12 +1,5 @@
 #include "UIInspector.h"
 
-#include "Cyph3D/Engine.h"
-#include "Cyph3D/Entity/Component/Animator.h"
-#include "Cyph3D/Entity/Entity.h"
-#include "Cyph3D/Rendering/Renderer/Renderer.h"
-#include "Cyph3D/Scene/Scene.h"
-#include "Cyph3D/Window.h"
-
 #include <imgui.h>
 
 IInspectable* UIInspector::_selected = nullptr;
@@ -28,33 +21,6 @@ void UIInspector::show()
 		if (_selected != nullptr)
 		{
 			_selected->onDrawUi();
-			
-			Entity* entity = dynamic_cast<Entity*>(_selected);
-			if (entity != nullptr)
-			{
-				ImGui::Spacing();
-				ImGui::Separator();
-				ImGui::Spacing();
-
-				float availableWidth = ImGui::GetWindowContentRegionWidth();
-				float buttonWidth = std::min(180.0f, availableWidth);
-				ImGui::SetCursorPosX(ImGui::GetCursorPosX() + ((availableWidth - buttonWidth) / 2));
-
-				if (ImGui::Button("Add Component", ImVec2(buttonWidth, 0)))
-					ImGui::OpenPopup("add_component");
-
-				if (ImGui::BeginPopup("add_component"))
-				{
-					for (auto it = Entity::allocators_begin(); it != Entity::allocators_end(); it++)
-					{
-						if (ImGui::Selectable(it->first.c_str()))
-						{
-							it->second(*entity);
-						}
-					}
-					ImGui::EndPopup();
-				}
-			}
 		}
 	}
 	
