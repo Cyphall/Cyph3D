a2afc788b2ca7d69490994e22bd5e006a9a02729
Cyphall <cyphallbraus@gmail.com>
2022-09-20 02:24:43 +0200

    fix: fixed ImGui color conversion

--- src/Cyph3D/UI/UIHelper.cpp
+++ src/Cyph3D/UI/UIHelper.cpp
@@ -116,7 +116,7 @@ void UIHelper::initDockingLayout(ImGuiID dockspaceId)
 
 static ImVec4 normalizeColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 {
-	return ImVec4(r / 255.99f, g / 255.99f, b / 255.99f, a / 255.99f);
+	return {r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f};
 }
 
 void UIHelper::initStyles()
