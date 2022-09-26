c1d4bc8d64ea6b354eca18e7793b5e1a1fa19fc6
Cyphall <cyphallbraus@gmail.com>
2022-09-26 02:22:06 +0200

    new: added utf8 <-> utf16 conversion helpers

--- src/Cyph3D/Helper/StringHelper.cpp
+++ src/Cyph3D/Helper/StringHelper.cpp
@@ -1,5 +1,9 @@
 #include "StringHelper.h"
 
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
 void StringHelper::remove(std::string& string, const char* stringToRemove)
 {
 	size_t pos = string.find(stringToRemove);
@@ -7,4 +11,32 @@ void StringHelper::remove(std::string& string, const char* stringToRemove)
 	{
 		string.erase(pos, strlen(stringToRemove));
 	}
+}
+
+std::string StringHelper::convert(const std::wstring& wstring)
+{
+	if (wstring.empty())
+	{
+		return {};
+	}
+	
+	int requiredSize = WideCharToMultiByte(CP_UTF8, 0, wstring.data(), wstring.size(), nullptr, 0, nullptr, nullptr);
+	std::string string(requiredSize, 0);
+	WideCharToMultiByte(CP_UTF8, 0, wstring.data(), wstring.size(), string.data(), string.size(), nullptr, nullptr);
+	
+	return string;
+}
+
+std::wstring StringHelper::convert(const std::string& string)
+{
+	if (string.empty())
+	{
+		return {};
+	}
+
+	int requiredSize = MultiByteToWideChar(CP_UTF8, 0, string.data(), string.size(), nullptr, 0);
+	std::wstring wstring(requiredSize, 0);
+	MultiByteToWideChar(CP_UTF8, 0, string.data(), string.size(), wstring.data(), wstring.size());
+
+	return wstring;
 }
--- src/Cyph3D/Helper/StringHelper.h
+++ src/Cyph3D/Helper/StringHelper.h
@@ -6,4 +6,6 @@ class StringHelper
 {
 public:
 	static void remove(std::string& string, const char* stringToRemove);
+	static std::string convert(const std::wstring& wstring);
+	static std::wstring convert(const std::string& string);
 };
