62f099219205acc899c74e8d6dd42f1b1c88bcbd
Cyphall <cyphallbraus@gmail.com>
2023-03-14 17:24:52 +0100

    misc: updated imgui to 1.89.4 and imguizmo to latest master

--- vendor/imgui/include/imconfig.h
+++ vendor/imgui/include/imconfig.h
@@ -92,6 +92,8 @@
         constexpr ImVec4(const glm::vec4& f) : x(f.x), y(f.y), z(f.z), w(f.w) {}   \
         operator glm::vec4() const { return glm::vec4(x,y,z,w); }
 
+//---- ...Or use Dear ImGui's own very basic math operators.
+//#define IMGUI_DEFINE_MATH_OPERATORS
 
 //---- Use 32-bit vertex indices (default is 16-bit) is one way to allow large meshes with more than 64K vertices.
 // Your renderer backend will need to support it (most example renderer backends support both 16/32-bit indices).
