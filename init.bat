64e00586687b88cc67fbd915120be7d046683b66
Cyphall <cyphallbraus@gmail.com>
2023-08-27 21:12:08 +0200

    misc: added back buildtrees deletion to save disk space

--- init.bat
+++ init.bat
@@ -6,7 +6,8 @@ git checkout cfdeb75bb67fee475b517a2f45d8f93f2b46bdb4
 call bootstrap-vcpkg.bat
 vcpkg install glfw3 glm nlohmann-json assimp stb magic-enum freetype sqlitecpp crossguid
 rmdir /s /q .git
-rmdir /s /q packages
+rmdir /s /q buildtrees
 rmdir /s /q downloads
+rmdir /s /q packages
 cd ..
 pause
