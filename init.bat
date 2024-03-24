706ed6a4e5192d63c595b33e674fa131b6bc04b4
Cyphall <cyphallbraus@gmail.com>
2024-03-24 16:29:12 +0100

    misc: Don't delete the buildtree directory to allow debugging lib code

--- init.bat
+++ init.bat
@@ -6,7 +6,6 @@ git checkout cd5e746ec203c8c3c61647e0886a8df8c1e78e41
 call bootstrap-vcpkg.bat
 vcpkg install glfw3 glm nlohmann-json assimp stb magic-enum freetype sqlitecpp crossguid
 rmdir /s /q .git
-rmdir /s /q buildtrees
 rmdir /s /q downloads
 rmdir /s /q packages
 cd ..
