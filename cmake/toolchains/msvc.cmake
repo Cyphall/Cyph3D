2eb6ba3ac2e28c49392be573ea00d9057a573323
Cyphall <cyphallbraus@gmail.com>
2026-05-18 19:26:00 +0200

    fix: fixed zlib compile error

--- cmake/toolchains/msvc.cmake
+++ cmake/toolchains/msvc.cmake
@@ -4,10 +4,8 @@ set(CMAKE_CXX_COMPILER cl)
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "/O2 /Ob3 /DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELEASE "/O2 /Ob3 /DNDEBUG")
 
-add_compile_options($<$<COMPILE_LANGUAGE:C>:/Zc:__STDC__>)
 add_compile_options($<$<COMPILE_LANGUAGE:C>:/Zc:__cplusplus>)
 add_compile_options($<$<COMPILE_LANGUAGE:C>:/utf-8>)
 
-add_compile_options($<$<COMPILE_LANGUAGE:CXX>:/Zc:__STDC__>)
 add_compile_options($<$<COMPILE_LANGUAGE:CXX>:/Zc:__cplusplus>)
 add_compile_options($<$<COMPILE_LANGUAGE:CXX>:/utf-8>)
