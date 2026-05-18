6644ede56de9a8aca486bcb714913545c3703633
Cyphall <cyphallbraus@gmail.com>
2026-05-18 19:26:20 +0200

    fix: fixed vcpkg root being randomly undefined

--- vcpkg/toolchains/linux-clang.cmake
+++ vcpkg/toolchains/linux-clang.cmake
@@ -1,4 +1,8 @@
 include("${CMAKE_CURRENT_LIST_DIR}/../../cmake/toolchains/clang.cmake")
 
-cmake_path(CONVERT "$ENV{VCPKG_ROOT}" TO_CMAKE_PATH_LIST VCPKG_ROOT)
+if (Z_VCPKG_ROOT_DIR)
+	set(VCPKG_ROOT ${Z_VCPKG_ROOT_DIR})
+elseif (_VCPKG_ROOT_DIR)
+	set(VCPKG_ROOT ${_VCPKG_ROOT_DIR})
+endif ()
 include("${VCPKG_ROOT}/scripts/toolchains/linux.cmake")
